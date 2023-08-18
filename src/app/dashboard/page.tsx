e5586d53ed1636e0d4389cd8ed303a49755113cc
Reni <renato@getbring.com>
2023-08-18 20:33:20 +0200

    just saving

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+// A awasome Dashboard page
+
+import React from 'react';
+
+export default  function DashboardPage() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+        </div>
+    );
+}
